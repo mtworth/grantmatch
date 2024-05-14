3413da79410d98636379508002a9d3b7049842bd
Maxwell T <43257438+mtworth@users.noreply.github.com>
2024-05-14 01:34:54 +0000

    testing

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+import os
 import streamlit as st
-st.write("Test")
+from streamlit_navigation_bar import st_navbar
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+page = st_navbar(["Home", "Find Grants", "About"],logo_path="/work/icons8-logo.svg")
+
+if page == "Home":
+    st.write("Welcome to the home page")
+if page == "Find Grants":
+    st.write("Welcome again")
