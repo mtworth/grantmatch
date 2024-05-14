027a4582cef770810e7e9171cf9187f985f40ab8
Maxwell T <43257438+mtworth@users.noreply.github.com>
2024-05-14 02:36:18 +0000

    minorupdates

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ if page == "Home":
     with col1: 
         #make this bold colored large font matching the brand primary color
         st.header("Grantr Match")
-        st.write("Using artificial intelligence to help match billions of dollars in federal grants to your projects and organizations")
+        st.write("Using artificial intelligence to help match billions of dollars in federal grants to your project and organization.")
         st.button("Find Grants",type="primary")
     with col2: 
         st.image("https://github.com/mtworth/grantr/blob/main/main/corp_art.jpg?raw=true")
@@ -22,16 +22,18 @@ if page == "Home":
     #logos evenly distributed 
 
     seal1, seal2, seal3, seal4, seal5 = st.columns(5)
+
+    w = 50
     with seal1:
-        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true")
+        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true",width=w)
     with seal2:
-        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true")
+        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true",width =w)
     with seal3:
-        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true")
+        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true",width=w)
     with seal4:
-        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true")
+        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true",width=w)
     with seal5:
-        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true")
+        st.image("https://github.com/mtworth/grantr/blob/main/images/NIH_Master_Logo_Vertical_2Color.png?raw=true",width=w)
 
     st.markdown('######')     
     col3, col4 = st.columns(2)
@@ -39,13 +41,13 @@ if page == "Home":
         st.image("https://github.com/mtworth/grantr/blob/main/main/corp_art.jpg?raw=true")
     with col4:
         st.subheader("Artificial Intelligence, Applied")
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+        st.write("We use state of the art generative AI to help remove the tedious work of searching grants and tell you whether a grant is the right match for you and your project.")
 
     st.markdown('##')     
     col5, col6 = st.columns(2)
     with col5:
         st.subheader("Fully Open Source")
-        st.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+        st.write("This entire application is built entirely on the Snowflake open source ecosystem, including Streamlit and Artic LLM. No need to worry about shady data pipelines and questionable AI.")
     with col6:
         st.image("https://github.com/mtworth/grantr/blob/main/main/corp_art.jpg?raw=true")
  
