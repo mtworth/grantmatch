753171acbcb5afb0503eafea380e71584fc0b5b7
Maxwell T <43257438+mtworth@users.noreply.github.com>
2024-05-21 05:07:24 +0000

    updated

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def download_and_process_data(url):
     else:
         return None, None
 
-@st.cache_data(allow_output_mutation=True)
+@st.cache_resource(allow_output_mutation=True)
 def load_model():
     return SentenceTransformer("Snowflake/snowflake-arctic-embed-m")
 
